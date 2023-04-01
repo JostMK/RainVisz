1d980f72acf992c7c789c91cc2a5a4762b051829
JostMK <jost.klink@gmail.com>
2023-04-01 18:28:18 +0200

    Adds orientation setting to variables.ini
    Skin now supports UP, DOWN, LEFT, RIGHT as orientation options

---
+++ @Resources/scripts/Orientate.lua
@@ -0,0 +1,31 @@
+-- @author Jost K. / GitHub: JostMK
+-- Sets the variables for the meters based of the orientation setting
+
+function Initialize()
+    local orientation = string.upper(SKIN:GetVariable('Orientation','UP'))
+
+    if orientation == 'DOWN' then
+        SKIN:Bang("!SetVariable", "BarX", "0")
+        SKIN:Bang("!SetVariable", "BarY", "0")
+        SKIN:Bang("!SetVariable", "BarW", "(#BarWidth#)")
+        SKIN:Bang("!SetVariable", "BarH", "(#BarWidth# + #BarHeight# * [MeasureBand{%%}])")
+    elseif orientation == 'LEFT' then
+        SKIN:Bang("!SetVariable", "MeterX", "0r")
+        SKIN:Bang("!SetVariable", "MeterY", "#BarPadding#R")
+
+        SKIN:Bang("!SetVariable", "BarX", "(#BarWidth# + #BarHeight#)")
+        SKIN:Bang("!SetVariable", "BarY", "0")
+        SKIN:Bang("!SetVariable", "BarW", "(-#BarWidth# - #BarHeight# * [MeasureBand{%%}])")
+        SKIN:Bang("!SetVariable", "BarH", "(#BarWidth#)")
+    elseif orientation == 'RIGHT' then
+        SKIN:Bang("!SetVariable", "MeterX", "0r")
+        SKIN:Bang("!SetVariable", "MeterY", "#BarPadding#R")
+
+        SKIN:Bang("!SetVariable", "BarX", "0")
+        SKIN:Bang("!SetVariable", "BarY", "0")
+        SKIN:Bang("!SetVariable", "BarW", "(#BarWidth# + #BarHeight# * [MeasureBand{%%}])")
+        SKIN:Bang("!SetVariable", "BarH", "(#BarWidth#)")
+    else
+        -- default case (UP) nothing to change
+    end
+end
--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # RainVisz
-An audio visualizer for [Rainmeter](https://www.rainmeter.net).
+An audio visualizer skin for [Rainmeter](https://www.rainmeter.net).
 
 ## Configuration
 For configuring the skin modify the variables of the [``variables.ini``](%40Resources/variables.ini) file in the [``@Resources``](%40Resources) folder.
